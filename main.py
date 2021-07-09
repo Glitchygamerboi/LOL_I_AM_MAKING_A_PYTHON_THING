04bf3044fb8cd9aebcf6d6d906fe3fc53575c45f
Glitchygamerboi <mayuukhb.student@mlsi.in>
2021-07-09 23:47:17 +0530

    lol im making a py project, where is this uploaded

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+number = 19
+number + 1
+20
+int('120')
+120
+
+random.randint(0, 100)
+
+
+print(number + 1)
+
+
+dir(5)
+
+class Lang:
+    isLolLangActivated = False
+
+    if isLolLangActivated:
+        swearsallowed = False
+        if swearsallowed:
+            print("NUWUPE")
+        else:
+            print("thank yu sw muwuch")
+        print("gib fud")
+        print("uwusual henlo wrld lul")
+    else:
+        swearsallowed = True
+        if swearsallowed:
+            print("NOPE")
+        else:
+            print("thank you SO mch")
+        print("give me food")
+        print("usual hello world lol")
