782b3200b2f59d8192af1298e9a9e1e5cf4bdd67
Glitchygamerboi <mayuukhb.student@mlsi.in>
2021-07-10 00:08:45 +0530

    yes, i coded stuff inside the py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import random
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 number = 19
 number + 1
 20
